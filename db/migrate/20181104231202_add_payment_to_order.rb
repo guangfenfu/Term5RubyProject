ed047c114beac4803a653ffdc55b6bbfde143dcb
Faye Fu <guangfenfu@gmail.com>
2018-11-04 17:44:14 -0600

    finished Product index, show, correct orders column. Next is Carrierwave

---
+++ db/migrate/20181104231202_add_payment_to_order.rb
@@ -0,0 +1,5 @@
+class AddPaymentToOrder < ActiveRecord::Migration[5.2]
+  def change
+    add_column :orders, :amount, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_164652) do
+ActiveRecord::Schema.define(version: 2018_11_04_231202) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2018_11_04_164652) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "amount"
   end
 
   create_table "product_snap_shots", force: :cascade do |t|
