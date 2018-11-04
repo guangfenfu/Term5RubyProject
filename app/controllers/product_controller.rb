f87191e5464c5570e959b3a2108f95cc3f10c6ad
GuangfenFu <guangfenfu@gmail.com>
2018-11-04 23:03:40 +0000

    created Product partial

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,6 @@
 class ProductController < ApplicationController
   def index
-    @product_collection = Product.order(:id).page params[:page]
+    @product_collection = Product.order(:name)
   end
 
   def show
