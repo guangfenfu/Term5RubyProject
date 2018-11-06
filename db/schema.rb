68883b5b640d0803da2b5957fc49b26d620caeed
GuangfenFu <guangfenfu@gmail.com>
2018-11-06 20:09:58 +0000

    added model ProductCategory for many to many Product and Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_194251) do
+ActiveRecord::Schema.define(version: 2018_11_06_200802) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 2018_11_06_194251) do
     t.decimal "amount"
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
   create_table "product_snap_shots", force: :cascade do |t|
     t.string "order_id"
     t.string "product_id"
@@ -87,12 +96,10 @@ ActiveRecord::Schema.define(version: 2018_11_06_194251) do
     t.decimal "price"
     t.string "image_link"
     t.text "description"
-    t.integer "category_id"
     t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "provinces", force: :cascade do |t|
