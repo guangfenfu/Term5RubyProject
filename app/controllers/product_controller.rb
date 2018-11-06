8a2d3d44376f5c93c0b51c2e974613773866c27c
GuangfenFu <guangfenfu@gmail.com>
2018-11-06 05:42:55 +0000

    finished nav, sider nav, starts on 2.2

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,7 @@
 class ProductController < ApplicationController
   def index
-    @product_collection = Product.order(:name)
+    @product_collection = Product.order(:name).page params[:page]
+    @categories = Category.all
   end
 
   def show
