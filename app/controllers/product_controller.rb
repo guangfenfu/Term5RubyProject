2ebd52e11a229a34bd75738cc720ee172cdc548a
Faye Fu <guangfenfu@gmail.com>
2018-11-09 01:25:46 -0600

    added search function

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,12 +2,23 @@ class ProductController < ApplicationController
   def index
     if params[:category].present?
       group = ProductCategory.where(category_id: params[:category]).map{|x| x.product_id}
-      @product_collection = Product.order(:name).where(id: group).page params[:page]
+      product_collection = Product.order(:name).where(id: group)
     else
-      @product_collection = Product.order(:name).page params[:page]
+      product_collection = Product.order(:name)
     end
 
+
+
+    if params[:search].present?
+      @product_collection = product_collection.where("name LIKE ? OR description LIKE ?", '%'+params[:search]+'%', '%'+params[:search]+'%').page params[:page]
+    else
+      @product_collection = product_collection.page params[:page]
+    end
+
+
+
     @categories = Category.all
+
   end
 
   def show
